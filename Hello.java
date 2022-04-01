c070eebdff9d4cc5fdc52cf919e934230db8e01e
arriving <38175006+arriving@users.noreply.github.com>
2022-04-01 16:18:03 +0800

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,16 @@
+package com.arriving.hello;
+
+public class Hello {
+
+    public static void main(String[] argc) {
+
+        Team team = new Team();
+        team.addMember(new Person("Tom"));
+        team.addMember(new Person("Jack"));
+
+        team.say("Hello");
+        if (argc.length > 0) {
+            team.say(argc[0]);
+        }
+    }
+}
