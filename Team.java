68fdd39b5ee3abd4772a7c11320fcb5f6f9cad47
arriving <38175006+arriving@users.noreply.github.com>
2022-04-01 16:19:27 +0800

    Create Team.java

---
+++ Team.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Team {
+
+    List<Person> members = new ArrayList<>();
+
+    public void addMember(Person person) {
+        this.members.add(person);
+    }
+
+    public void say(String word) {
+        for (Person member : members) {
+            member.say(word);
+        }
+    }
+}
